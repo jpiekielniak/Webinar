5abdb6b5545fb09c943a830da7c7e1f01b34d567
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-11-14 15:06:33 +0100

    change: make payment process model bpmn

--- src/main/java/org/example/webinar/bmpn/PaymentProcessController.java
+++ src/main/java/org/example/webinar/bmpn/PaymentProcessController.java
@@ -17,14 +17,18 @@ import java.util.Map;
 public class PaymentProcessController {
 
     @Qualifier("zeebeClientLifecycle")
+
     private ZeebeClient client;
+    private static final String MESSAGE = "paymentRequestMessage";
 
     @PostMapping("/payment")
     public Map<String, Object> startPayment(@RequestBody Map<String, Object> variables) {
 
+        final var correlationKey = variables.get("processInstanceKey").toString();
+
         client.newPublishMessageCommand()
-                .messageName("paymentRequestMessage")
-                .correlationKey(variables.get("processInstanceKey").toString())
+                .messageName(MESSAGE)
+                .correlationKey(correlationKey)
                 .variables(variables)
                 .send()
                 .join();
